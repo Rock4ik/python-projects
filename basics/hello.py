0bb315365518f9a2e551b6626951d51e76cb7efb
Rock4ik <ilnazclass@gmail.com>
2025-08-31 02:26:28 +0300

    Delete basics directory

--- basics/hello.py
+++
@@ -1,2 +0,0 @@
-print("Hello, World!")
-print("Мой первый коммит!")
--- basics/print.py
+++
@@ -1,9 +0,0 @@
-print("Hello", "World")
-print("Hello", "World", sep=", ")
-print("first row", end= " ")
-print("second row")
-
-###
-
-name = input("Enter your name: ")
-print(name)
