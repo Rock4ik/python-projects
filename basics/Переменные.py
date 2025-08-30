433e5943e7a2e84f5600fb1364e6cf7133ee0380
Rock4ik <ilnazclass@gmail.com>
2025-08-31 00:01:43 +0300

    Add files via upload

---
+++ basics/Переменные.py
@@ -0,0 +1,20 @@
+name = "Alice"
+age = 25
+print(name, age)
+
+###
+
+x = 3
+y = 4
+
+x, y = y, x
+print(x, y)
+
+###
+
+variable = 1
+print(type(variable))
+
+first_number = 1
+two_number = 2
+print(first_number + two_number)
